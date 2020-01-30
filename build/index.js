8c237f307df5533b3229b54483f3bcd79ad61176
schema-generator <developer@techx.vn>
2020-01-30 15:31:08 +0700

    schema genarator by buildNumber:

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const userListRequest_1 = require("./userListRequest");
+exports.userListRequestSchema = userListRequest_1.userListRequestSchema;
+exports.userListRequestValidator = userListRequest_1.userListRequestValidator;
+const userListResponse_1 = require("./userListResponse");
+exports.userListResponseSchema = userListResponse_1.userListResponseSchema;
+exports.userListResponseValidator = userListResponse_1.userListResponseValidator;
+//# sourceMappingURL=index.js.map
---
+++ build/userListResponse.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Ajv = require("ajv");
+exports.userListResponseSchema = {
+    "id": "http://tradex.techx.vn/user/userListResponse",
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "userListResponse schema",
+    "type": "object",
+    "properties": {
+        "id": {
+            "description": "userId",
+            "type": "integer"
+        },
+        "primaryAccountType": {
+            "description": "primary_account_type",
+            "type": "string"
+        },
+        "username": {
+            "description": "username",
+            "type": "string"
+        },
+        "displayName": {
+            "description": "display_name",
+            "type": "string"
+        },
+        "email": {
+            "description": "email",
+            "type": "string"
+        },
+        "phoneCode": {
+            "description": "phone_code",
+            "type": "string"
+        },
+        "phoneNumber": {
+            "description": "phone_number",
+            "type": "string"
+        },
+        "avatar": {
+            "description": "avatar",
+            "type": "string"
+        },
+        "domain": {
+            "description": "domain",
+            "type": "string"
+        },
+        "birthday": {
+            "description": "birthday",
+            "type": "string"
+        }
+    }
+};
+let userListResponseAjv = null;
+function userListResponseValidator() {
+    if (userListResponseAjv == null) {
+        userListResponseAjv = new Ajv({ validateSchema: false }).compile(exports.userListResponseSchema);
+    }
+    return userListResponseAjv;
+}
+exports.userListResponseValidator = userListResponseValidator;
+//# sourceMappingURL=userListResponse.js.map
