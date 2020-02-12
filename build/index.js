7c72a7832062d91dd220b1a51c801a87d078ae5f
schema-generator <developer@techx.vn>
2020-02-12 02:55:47 +0000

    schema genarator by buildNumber: 222

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userListRequest_1 = require("./userListRequest");
-exports.userListRequestSchema = userListRequest_1.userListRequestSchema;
-exports.userListRequestValidator = userListRequest_1.userListRequestValidator;
-const userListResponse_1 = require("./userListResponse");
-exports.userListResponseSchema = userListResponse_1.userListResponseSchema;
-exports.userListResponseValidator = userListResponse_1.userListResponseValidator;
-//# sourceMappingURL=index.js.map
