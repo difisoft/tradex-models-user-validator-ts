5be421fac4770ff031c13a852e307d13e37ae62e
Developer <developer@techx.vn>
2020-05-13 04:31:14 +0000

    schema genarator by buildNumber: 321

--- build/userListRequest.js
+++ build/userListRequest.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.userListRequestValidator = exports.userListRequestSchema = void 0;
 const Ajv = require("ajv");
 exports.userListRequestSchema = {
     "id": "http://tradex.techx.vn/user/userListRequest",
--- build/userListResponse.js
+++ build/userListResponse.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.userListResponseValidator = exports.userListResponseSchema = void 0;
 const Ajv = require("ajv");
 exports.userListResponseSchema = {
     "id": "http://tradex.techx.vn/user/userListResponse",
