f1dbbedefaa7e6ef1103f6a67804a3c294de98a1
Developer <developer@techx.vn>
2020-10-19 04:13:09 +0000

    schema genarator by buildNumber:

---
+++ build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userListResponseValidator = exports.userListResponseSchema = exports.userListRequestValidator = exports.userListRequestSchema = void 0;
+const userListRequest_1 = require("./userListRequest");
+Object.defineProperty(exports, "userListRequestSchema", { enumerable: true, get: function () { return userListRequest_1.userListRequestSchema; } });
+Object.defineProperty(exports, "userListRequestValidator", { enumerable: true, get: function () { return userListRequest_1.userListRequestValidator; } });
+const userListResponse_1 = require("./userListResponse");
+Object.defineProperty(exports, "userListResponseSchema", { enumerable: true, get: function () { return userListResponse_1.userListResponseSchema; } });
+Object.defineProperty(exports, "userListResponseValidator", { enumerable: true, get: function () { return userListResponse_1.userListResponseValidator; } });
+//# sourceMappingURL=index.js.map
